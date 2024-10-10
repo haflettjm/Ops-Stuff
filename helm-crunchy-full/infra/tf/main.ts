862a1220e99c461811a56afbd602b87acf82871a
Jacob Haflett <hafletjm01@gmail.com>
2024-10-10 16:56:31 -0400

    more stuff for the prototype

--- helm-crunchy-full/infra/tf/main.ts
+++ helm-crunchy-full/infra/tf/main.ts
@@ -1,14 +1,31 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { provider, computeInstance, serviceAccount } from "@cdktf/provider-google";
+import { logger, readDir } from "./util";
+import { ProjectInfo } from "./process_object";
 
-class MyStack extends TerraformStack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+class CreateComputInstanceStack extends TerraformStack {
+  constructor(scope: Construct, name: string, projectInfo: ProjectInfo ) {
+    super(scope, name);
+    new provider.GoogleProvider(this, `provider-${projectInfo.projectMeta?.project}-${projectInfo.projectMeta?.region}`,  projectInfo.projectMeta);
 
-    // define resources here
+    const sa = new serviceAccount.ServiceAccount(this, "default", {
+      accountId: "cdktf-accountid",
+      displayName: "Service Account",
+      description: "Created by CDKTF"
+    })
+
+    projectInfo.gcpObjects?.forEach(gcpObject => {
+      gcpObject.instances.forEach(instance => {
+        instance.serviceAccount.email = sa.email
+        new computeInstance.ComputeInstance(this, instance.name, instance);
+      });     
+    });
   }
 }
 
+const projectInfo = readDir('gcp_projects/dev-projects/haflettjm/');
+logger.info(projectInfo);
 const app = new App();
-new MyStack(app, "tf");
+new CreateComputInstanceStack(app, "cdktf_poc", projectInfo);
 app.synth();
---
+++ helm-crunchy-full/infra/tf/util.ts
@@ -0,0 +1,39 @@
+import { load as loadYaml } from "js-yaml";
+import { GCPObjects, ProjectInfo } from './process_object';
+import { resolve } from 'path';
+const log4js = require("log4js");
+const fs = require('fs');
+
+log4js.configure({
+  appenders: { projectLogger: {type: "file", filename:"typescriptL.log"} },
+  categories: { default: { appenders: ["projectLogger"], level:"info"} }
+});
+
+export const logger = log4js.getLogger();
+
+export function readYamlFileSync(filePath:String): any{
+  return loadYaml(fs.readYamlFileSync(filePath, 'utf-8'));
+}
+
+export function readDir(dirPath:string): ProjectInfo {
+  let projectInfo: ProjectInfo = {};
+  let gcpObjects: GCPObjects[] = [];
+  try {
+    logger.info("DirPath is: ", dirPath);
+    
+    fs.readdirSync(dirPath).forEach((file: string) => {
+      const resolvedFilePath = resolve(dirPath, file);
+      logger.info(resolvedFilePath);
+      if(file.includes("project.yaml")){
+        projectInfo = (<ProjectInfo> readYamlFileSync(resolvedFilePath));
+      }else{
+        gcpObjects.push(<GCPObjects> readYamlFileSync(resolvedFilePath));
+      }
+    });
+    projectInfo.gcpObjects = gcpObjects;
+    logger.info("ProjectInfo: ", projectInfo);
+  } catch (error){
+    logger.error(error);
+  }
+  return projectInfo;
+}
